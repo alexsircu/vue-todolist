9b36a81f0531399bf7ad1fe4c701dc1d552ef9cf
Alex Sircu <alex.sricu@gmail.com>
2020-12-10 16:11:11 +0100

    Lista, tasto aggiungi e funzione che aggiunge item

---
+++ js/script.js
@@ -0,0 +1,29 @@
+/* Esercizio:
+- creare una lista di cose da fare e mostrarla in pagina con Vue;
+- aggiungere un campo di input e un pulsante per aggiungere nuove cose da fare alla lista;
+- cancellare un item dalla lista, al click sulla relativa icona 'X'.
+Bonus:
+- Oltre al click sul pulsante, consentire l'inserimento del nuovo item anche premendo il tasto Invio della tastiera, dopo aver scritto il testo nel campo di input.
+Consiglio del giorno: per alcuni task, potrebbe essere necessario fare delle ricerche e capire come lavorano alcune funzioni o direttive leggendo le documentazioni ufficiali ed analizzando i relativi esempi. */
+
+var app = new Vue({
+
+  el: "#root",
+  data: {
+    list: [
+      "Segliarsi alle 7:00",
+      "Fare la spesa",
+      "Fare un giro in bici",
+      "Leggere 10 pagine del libro"
+    ]
+  },
+  methods: {
+    addItem: function() {      
+      let newItem = this.$refs.writeItem.value;
+      this.list.push(newItem);
+    }
+
+  }
+
+
+});
